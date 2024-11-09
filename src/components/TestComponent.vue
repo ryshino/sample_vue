2315bd970acfe314b6fe97dd3b3707d4ed09a443
ryshino <ranature1153@gmail.com>
2024-11-09 19:24:30 +0900

    dataを定義する

--- src/components/TestComponent.vue
+++ src/components/TestComponent.vue
@@ -1,9 +1,18 @@
 <script setup>
+import { ref } from 'vue'
+const name = ref('')
 defineProps({
   text: String,
 })
+const dataTest = 'これはデータです！'
 </script>
 
 <template>
   <p>{{ text }}</p>
+  <p>{{ dataTest }}</p>
+  <label>
+    <span>名前</span>
+    <input type="text" v-model="name" />
+  </label>
+  <p>ここの値が変わるよ→{{ name }}</p>
 </template>
