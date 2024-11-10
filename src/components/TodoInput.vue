7fff58490d5314a86831d0ba344456b1ab9dd84c
ryshino <ranature1153@gmail.com>
2024-11-10 12:08:23 +0900

    フォームを作成する

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+const input = ref('')
+const inputDate = ref('')
+
+function onSubmitForm() {
+  console.log(input.value)
+}
+</script>
+
+<template>
+  <div>
+    <form>
+      <label>やること<input type="text" v-model="input" /></label>
+      <label>期限<input type="date" v-model="inputDate" /></label>
+      <input type="submit" @click="onSubmitForm" value="登録！" />
+    </form>
+  </div>
+</template>
---
+++ src/components/TodoListView.vue
@@ -0,0 +1,9 @@
+<!-- TodoListView.vue -->
+<script setup lang="ts">
+// ここにスクリプトを書く
+</script>
+
+<template>
+  <!-- ここにテンプレートを書く -->
+  <div>TodoListView</div>
+</template>
